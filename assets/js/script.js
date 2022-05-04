600ac6a871729a55594f5574227c1e95b97dd943
Mark Pavel <markpavel02@gmail.com>
2022-05-04 15:22:40 -0400

    5-Day Forcast And Final Stylings

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const APIKEY = "a08c74698b6f4ec795426b447ed75b26"
+const APIKEY = "a08c74698b6f4ec795426b447ed75b26";
 var searchHistory = [];
 
 function addHistory(name, lon, lat) {
@@ -31,29 +31,70 @@ function addHistory(name, lon, lat) {
     }
 }
 function getWeather(name, lon, lat) {
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIKEY}`).then(function(response) {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=imperial&appid=${APIKEY}`).then(function(response) {
         response.json().then(function(data) {
+            var uvi = data.daily[0].uvi;
+
             $("#today")
             .empty()
             .append(
                 $("<h3>")
-                .text(name + new Date(data.current.dt).toISOString().substring(0, 10)),
+                .text(name + " " + new Date(data.daily[0].dt * 1000).toISOString().substring(0, 10)),
 
                 $("<h4>")
-                .text("Temp: " + data.current.temp + "° F"),
+                .text("Temp: " + data.daily[0].temp.day + "° F"),
 
                 $("<h4>")
-                .text("Wind: " + data.current.wind_speed + "MPH"),
+                .text("Wind: " + data.daily[0].wind_speed + "MPH"),
 
                 $("<h4>")
-                .text("Humidity " + data.current.humidity + "%")
-            )
-        })
-    })
+                .text("Humidity " + data.daily[0].humidity + "%"),
 
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=5&appid=${APIKEY}`).then(function(response) {
-        response.json().then(function(data) {
-            console.log(new Date(data.list[0].dt).toISOString())
+                $("<h4>")
+                .text("UV Index: ")
+                .append(
+                    $("<span>")
+                    .text(uvi)
+                    .attr("id", "uvi")
+                )
+            );
+
+            if (uvi < 3) {
+                $("#uvi").addClass("green")
+            } else if (uvi >= 3 && uvi < 6) {
+                $("#uvi").addClass("yellow")
+            } else if (uvi >= 6 && uvi < 8) {
+                $("#uvi").addClass("orange")
+            } else if (uvi >=8 && uvi < 10) {
+                $("#uvi").addClass("red")
+            } else if (uvi > 9) {
+                $("#uvi").addClass("purple text-light")
+            }
+
+            $("#5-day").empty();
+
+            for (var x = 0; x < 5; x++) {
+                $("#5-day").append(
+                    $("<div>")
+                    .addClass("card d-flex").append(
+                        $("<h3>")
+                        .addClass("card-header bg-dark text-light")
+                        .text(new Date(data.daily[x + 1].dt * 1000).toISOString().substring(0, 10)),
+
+                        $("<div>")
+                        .addClass("card-body bg-secondary").append(
+                            $("<h4>")
+                            .text("Temp: " + data.daily[x + 1].temp.day + "° F"),
+
+                            $("<h4>")
+                            .text("Wind: " + data.daily[x + 1].wind_speed + "MPH"),
+            
+                            $("<h4>")
+                            .text("Humidity " + data.daily[x + 1].humidity + "%")
+                        )
+                    )
+                )
+            }
         })
     })
 }
